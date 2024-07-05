99b31c690f474a038c3b7a49fc4c6838e00389f9
Nguyen-Ngoc-Linht <ngoclinhtp2002@gmail.com>
2024-07-06 00:42:14 +0700

    done product page

---
+++ public/Crc-60Mhm-166.php
@@ -0,0 +1,131 @@
+<?php include('../includes/header.php') ?>
+<link rel="stylesheet" href="css/Crc-60Mhm-166.css">
+<main class="center">
+    <div class="slogan top-main center column">
+        <h1>CRC 60MHM - 166</h1>
+        <p>Half-cut Modules</p>
+    </div>
+
+    <div class="main center column">
+        <div class="row column center">
+            <div class="container">
+                <div class="title-container">
+                    <h1>CRC 60MHM - 166</h1>
+                </div>
+            </div>
+
+            <div class="container row">
+                <div class="col-6">
+                    <div class="box-image">
+                        <img src="./images/CRC66MHM.jpg" alt="" class="image">
+                    </div>
+                </div>
+
+                <div class="col-6">
+                    <div class="box-content">
+                        <h1>Thông tin sản phẩm</h1>
+                        <p>166 Series</p>
+                        <div class="row info-product">
+                            <div class="col-5">
+                                <p>Công nghệ</p>
+                            </div>
+                            <div class="col-5">
+                                <p>Half-cut, PERC</p>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-5">
+                                <p>Tổng số Cell</p>
+                            </div>
+                            <div class="col-5">
+                                <p> 120</p>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-5">
+                                <p>Dải công suất</p>
+                            </div>
+                            <div class="col-5">
+                                <p>370W - 390W</p>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-5">
+                                <p>Điện áp hệ thống lớn nhất</p>
+                            </div>
+                            <div class="col-5">
+                                <p>1500V</p>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-5">
+                                <p>Hiệu suất tấm pin lớn nhất</p>
+                            </div>
+                            <div class="col-5">
+                                <p>21.28%</p>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-5">
+                                <p>Kích thước</p>
+                            </div>
+                            <div class="col-5">
+                                <p>1765x1038x35mm</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="container row">
+                <div class="row center">
+                    <h1 class="title-demo">Demo ảnh sản phẩm</h1>
+                </div>
+            </div>
+
+            <div class="container row">
+                <div class="col-4">
+                    <div class="image-demo">
+                        <img src="./images/demo1.jpg" alt="" class="demo-img">
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="image-demo">
+                        <img src="./images/demo2.jpg" alt="" class="demo-img">
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="image-demo">
+                        <img src="./images/demo3.jpg" alt="" class="demo-img">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Map -->
+        <div class="map-container center">
+            <div class="block block2 row">
+                <div class="map">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1863.877181162358!2d105.54829056814654!3d20.881948810981985!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x313445b324859dd3%3A0x1ee430bc9947d8e6!2zS0NOIEzGsMahbmcgU8ahbg!5e0!3m2!1svi!2s!4v1719662674795!5m2!1svi!2s" width="100%" height="400" style="border: var(--border);" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+                <div class="extra-info column">
+                    <ul class="social-container row">
+                        <li class="social center"><i class="fa-brands fa-tiktok"></i></li>
+                        <li class="social center"><i class="fa-brands fa-facebook-f"></i></li>
+                        <li class="social center"><i class="fa-brands fa-youtube"></i></li>
+                        <li class="social center"><i class="fa-brands fa-twitter"></i></li>
+                    </ul>
+                    <div class="logo">
+                        <img src="images/logo.png" alt="" srcset="">
+                    </div>
+                    <h1>CRC GROUP</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+<?php include('../includes/footer.php') ?>
--- public/technology.php
+++ public/technology.php
@@ -10,96 +10,132 @@
             <label>Công nghệ PERC</label>
         </div>
 
-        <div class="row center box-technno">
-            <div class="container">
-                <div class="col-7 left">
-                    <p>Dựa trên tấm nền đơn tinh thể tiên tiến và công nghệ chống LID, CRC Solar đảm bảo bảo hành điện năng năm đầu tiên hơn ≥98% cho các mô-đun PV.</p>
-                </div>
-                <div class="col-5 right">
-                    <img src="./images/technology1.png" alt="" class="img-techno">
-                </div>
-            </div>
-        </div>
-
-        <div class="line-white"></div>
-
-        <div class="row center box-technno">
+        <div class="row center column">
             <div class="container">
                 <div class="col-5 left">
-                    <img src="./images/technology2.png" alt="" class="img-techno">
+                    <div class="box-content">
+                        <div class="top-block">
+                            <span>Tấm nền đơn tinh thể</span>
+                        </div>
+                        <h1>Tấm nền đơn tinh thể</h1>
+                        <p>Dựa trên tấm nền đơn tinh thể tiên tiến và công nghệ chống LID, CRC Solar</p>
+                        <p>Đảm bảo bảo hành điện năng năm đầu tiên hơn ≥98% cho các mô-đun PV.</p>
+                    </div>
                 </div>
                 <div class="col-7 right">
-                    <p>Hiệu suất bức xạ thấp vượt trội, hệ số nhiệt độ điện năng thấp, nhiệt độ hoạt động thấp, tất cả những công nghệ này dẫn đến năng suất năng lượng cao cho các mô-đun năng lượng mặt trời của CRC Solar.</p>
+                    <div class="box-image">
+                        <img src="./images/technology1.png" alt="" class="img-techno">
+                    </div>
                 </div>
             </div>
-        </div>
 
-        <div class="line-white"></div>
+            <div class="line-white"></div>
 
-
-        <div class="row center box-technno">
             <div class="container">
                 <div class="col-7 left">
-                    <p>Đối với tế bào PERC hai mặt, trường bề mặt sau Al được thay thế bằng lưới Al, do đó làm cho phần lớn mặt sau trong suốt, cho phép tế bào hấp thụ nhiều ánh sáng mặt trời hơn và tạo ra nhiều điện hơn từ cả hai mặt.</p>
+                    <div class="box-image">
+                        <img src="./images/technology2.png" alt="" class="img-techno">
+                    </div>
                 </div>
                 <div class="col-5 right">
-                    <img src="./images/technology3.jpg" alt="" class="img-techno">
+                    <div class="box-content">
+                        <div class="top-block">
+                            <span>Hiệu suất bức xạ</span>
+                        </div>
+                        <h1>Hiệu suất bức xạ</h1>
+                        <p>Hiệu suất bức xạ thấp vượt trội, hệ số nhiệt độ điện năng thấp, nhiệt độ hoạt động thấp</p>
+                        <p>tất cả những công nghệ này dẫn đến năng suất năng lượng cao cho các mô-đun năng lượng mặt trời của CRC Solar</p>
+                    </div>
                 </div>
             </div>
-        </div>
 
-        <div class="line-white"></div>
+            <div class="line-white"></div>
 
-        <div class="row center box-technno">
             <div class="container">
                 <div class="col-5 left">
-                    <img src="./images/technology4.jpg" alt="" class="img-techno">
+                    <div class="box-content">
+                        <div class="top-block">
+                            <span>Tấm nền đơn tinh thể</span>
+                        </div>
+                        <h1>Tấm nền đơn tinh thể</h1>
+                        <p>Dựa trên tấm nền đơn tinh thể tiên tiến và công nghệ chống LID, CRC Solar</p>
+                        <p>Đảm bảo bảo hành điện năng năm đầu tiên hơn ≥98% cho các mô-đun PV.</p>
+                    </div>
                 </div>
                 <div class="col-7 right">
-                    <p>Tế bào PERC có mặt sau bị động và được tăng cường bởi quá trình tạo rãnh bằng laser, giúp cải thiện đáng kể hiệu quả của tế bào.</p>
+                    <div class="box-image">
+                        <img src="./images/technology1.png" alt="" class="img-techno">
+                    </div>
+                </div>
+            </div>
+
+            <div class="line-white"></div>
+
+            <div class="container">
+                <div class="col-7 left">
+                    <div class="box-image">
+                        <img src="./images/technology2.png" alt="" class="img-techno">
+                    </div>
+                </div>
+                <div class="col-5 right">
+                    <div class="box-content">
+                        <div class="top-block">
+                            <span>Hiệu suất bức xạ</span>
+                        </div>
+                        <h1>Hiệu suất bức xạ</h1>
+                        <p>Hiệu suất bức xạ thấp vượt trội, hệ số nhiệt độ điện năng thấp, nhiệt độ hoạt động thấp</p>
+                        <p>tất cả những công nghệ này dẫn đến năng suất năng lượng cao cho các mô-đun năng lượng mặt trời của CRC Solar</p>
+                    </div>
                 </div>
             </div>
         </div>
 
-        <div class="line-white"></div>
-
-        <div class="row center column" style="background-color: white;">
+        <div class="row center column box-content-2">
             <div class="row center column title-technology">
                 <h1 class="">Phát điện mặt trời từ mô-đun hai mặt</h1>
                 <label>Phát điện mặt trời từ mô-đun hai mặt</label>
             </div>
 
-            <div class="row center box-technno">
-                <div class="container">
-                    <div class="col-7 left">
-                        <p>Sản lượng năng lượng của mô-đun hai mặt có thể bị ảnh hưởng bởi độ trắng xóa (albedo),
-                            chiều cao của mô-đun, tỷ lệ phủ quang mặt đất (GCR) và độ chiếu xạ bình thường trực tiếp
-                            (DHI), v.v. Chiều cao lắp đặt của mô-đun hai mặt nên cao hơn 1m. Cần tránh che bóng từ giá
-                            đỡ và hộp nối. Hiện tại, việc phát điện của mô-đun hai mặt trên giá đỡ cố định và bộ theo dõi
-                            trục đơn có thể được mô phỏng bằng PVsyst. Nhà đầu tư có thể xác định tỷ lệ DC / AC của hệ thống mô-đun hai mặt để giảm thiểu LCOE.</p>
+            <div class="container">
+                <div class="col-5 left">
+                    <div class="box-content">
+                        <div class="top-block">
+                            <span style="background-color: white;">Sản lượng năng lượng</span>
+                        </div>
+                        <h1>Sản lượng năng lượng</h1>
+                        <p>Sản lượng năng lượng của mô-đun hai mặt có thể bị ảnh hưởng bởi độ trắng xóa (albedo), chiều cao của mô-đun, tỷ lệ phủ quang mặt đất (GCR) và độ chiếu xạ bình thường trực tiếp (DHI), v.v</p>
+                        <p>Chiều cao lắp đặt của mô-đun hai mặt nên cao hơn 1m</p>
+                        <p> Cần tránh che bóng từ giá đỡ và hộp nối. Hiện tại, việc phát điện của mô-đun hai mặt trên giá đỡ cố định và bộ theo dõi trục đơn có thể được mô phỏng bằng PVsyst</p>
+                        <p>Nhà đầu tư có thể xác định tỷ lệ DC / AC của hệ thống mô-đun hai mặt để giảm thiểu LCOE</p>
                     </div>
-                    <div class="col-5 right">
+                </div>
+                <div class="col-7 right">
+                    <div class="box-image">
                         <img src="./images/technology5.jpg" alt="" class="img-techno">
                     </div>
                 </div>
             </div>
         </div>
 
-        <div class="row center column" style="padding-bottom: 60px;">
+        <div class="row center column box-content-3">
             <div class="row center column title-technology">
                 <h1 class="">Công nghệ Half-cut</h1>
                 <label>Công nghệ Half-cut</label>
             </div>
 
-            <div class="row center box-technno">
-                <div class="container">
-                    <div class="col-7 left">
-                        <p>Trong điều kiện chiếu xạ cao, mô-đun nửa tế bào (Half-Cell),
-                            đặc biệt là mô-đun nửa tế bào hai mặt (Bifacial Half-Cell),
-                            sẽ có năng suất cao hơn so với mô-đun thông thường.
-                            Mô-đun nửa ô hai mặt (Bifacial Half-cell) sẽ giúp đạt được LCOE thấp nhất ở những khu vực có nhiều bức xạ mặt trời.</p>
+            <div class="container">
+                <div class="col-5 left">
+                    <div class="box-content">
+                        <div class="top-block">
+                            <span>Điều kiện chiếu xạ</span>
+                        </div>
+                        <h1>Điều kiện chiếu xạ</h1>
+                        <p>Trong điều kiện chiếu xạ cao, mô-đun nửa tế bào (Half-Cell), đặc biệt là mô-đun nửa tế bào hai mặt (Bifacial Half-Cell), sẽ có năng suất cao hơn so với mô-đun thông thường</p>
+                        <p>Mô-đun nửa ô hai mặt (Bifacial Half-cell) sẽ giúp đạt được LCOE thấp nhất ở những khu vực có nhiều bức xạ mặt trời.</p>
                     </div>
-                    <div class="col-5 right">
+                </div>
+                <div class="col-7 right">
+                    <div class="box-image">
                         <img src="./images/technology6.png" alt="" class="img-techno">
                     </div>
                 </div>
@@ -107,67 +143,66 @@
 
             <div class="line-white"></div>
 
-            <div class="row center box-technno">
-                <div class="container">
-                    <div class="col-5 left">
+            <div class="container">
+                <div class="col-7 left">
+                    <div class="box-image">
                         <img src="./images/technology7.png" alt="" class="img-techno">
                     </div>
-                    <div class="col-7 right">
-                        <p>Do thiết kế kết nối song song độc đáo,
-                            các mô-đun nửa tế bào (Half-cell) vẫn có công suất 50% trong điều kiện
-                            che bóng khi bình minh hoặc hoàng hôn khi cài đặt chân dung.</p>
-                        <p>Ngoài ra, công nghệ cắt một nửa (Half-Cut) có thể cải thiện đầu
-                            ra của mô-đun hai mặt (Bifacial) dưới điều kiện ánh sáng phổ quang không đồng nhất ở mặt sau.
-                        </p>
+                </div>
+                <div class="col-5 right">
+                    <div class="box-content">
+                        <div class="top-block">
+                            <span>Thiết kế đối song song</span>
+                        </div>
+                        <h1>Thiết kế đối song song</h1>
+                        <p>Do thiết kế kết nối song song độc đáo, các mô-đun nửa tế bào (Half-cell) vẫn có công suất 50% trong điều kiện che bóng khi bình minh hoặc hoàng hôn khi cài đặt chân dung</p>
+                        <p>Ngoài ra, công nghệ cắt một nửa (Half-Cut) có thể cải thiện đầu ra của mô-đun hai mặt (Bifacial) dưới điều kiện ánh sáng phổ quang không đồng nhất ở mặt sau</p>
                     </div>
                 </div>
             </div>
 
             <div class="line-white"></div>
 
-            <div class="row center box-technno">
-                <div class="container">
-                    <div class="col-7 left">
-                        <p>Các tế bào được cắt một nửa (Half-cell) tiết kiệm một nửa dòng điện đang chạy, do đó tổn thất nhiệt được giảm đáng kể. Nhiệt độ hoạt động giảm tương ứng, và độ chắc chắn của mô-đun được cải thiện cùng với sự tăng công suất.</p>
+            <div class="container">
+                <div class="col-5 left">
+                    <div class="box-content">
+                        <div class="top-block">
+                            <span>Cắt đôi tế bào</span>
+                        </div>
+                        <h1>Cắt đôi tế bào</h1>
+                        <p>Các tế bào được cắt một nửa (Half-cell) tiết kiệm một nửa dòng điện đang chạy, do đó tổn thất nhiệt được giảm đáng kể</p>
+                        <p>Nhiệt độ hoạt động giảm tương ứng, và độ chắc chắn của mô-đun được cải thiện cùng với sự tăng công suất</p>
                     </div>
-                    <div class="col-5 right">
-                        <img src="./images/technology8.png" alt="" class="img-techno">
+                </div>
+                <div class="col-7 right">
+                    <div class="box-image">
+                        <img src="./images//technology8.png" alt="" class="img-techno">
                     </div>
                 </div>
             </div>
 
             <div class="line-white"></div>
 
-            <div class="row center box-technno">
-                <div class="container">
-                    <div class="col-5 left">
-                        <img src="./images/technology9.png" alt="" class="img-techno">
+            <div class="container">
+                <div class="col-7 left">
+                    <div class="box-image">
+                        <img src="./images/technology9.png" alt="Ung dung thuc dia" class="img-techno">
                     </div>
-                    <div class="col-7 right">
-                        <p>Trong các ứng dụng thực địa, các tấm che bóng có diện tích nhỏ có thể khiến nhiệt độ của các bộ phận đó tăng lên rất cao. Hiện tượng này được gọi là điểm nóng. Thời gian dài của điểm nóng có thể dẫn đến sự xuống cấp không thể phục hồi của các mô-đun.</p>
-                        <p>Bởi vì dòng điện chuỗi của mô-đun nửa tế bào (Half-cell) bằng một nửa của mô-đun toàn tế bào (Full-cell), nhiệt độ điểm nóng rõ ràng có thể giảm xuống. Các thí nghiệm của CRC Solar cho thấy mức giảm này có thể là 10-20 ℃, giúp tăng độ tin cậy của mô-đun.
-                        </p>
+                </div>
+                <div class="col-5 right">
+                    <div class="box-content">
+                        <div class="top-block">
+                            <span>Ứng dụng thực địa</span>
+                        </div>
+                        <h1>Ứng dụng thực địa</h1>
+                        <p>Trong các ứng dụng thực địa, các tấm che bóng có diện tích nhỏ có thể khiến nhiệt độ của các bộ phận đó tăng lên rất cao. Hiện tượng này được gọi là điểm nóng.</p>
+                        <p>Thời gian dài của điểm nóng có thể dẫn đến sự xuống cấp không thể phục hồi của các mô-đun.</p>
+                        <p>Bởi vì dòng điện chuỗi của mô-đun nửa tế bào (Half-cell) bằng một nửa của mô-đun toàn tế bào (Full-cell), nhiệt độ điểm nóng rõ ràng có thể giảm xuống</p>
+                        <p>Các thí nghiệm của CRC Solar cho thấy mức giảm này có thể là 10-20 ℃, giúp tăng độ tin cậy của mô-đun</p>
                     </div>
                 </div>
             </div>
-
-            <div class="line-white"></div>
-
-            <div class="row center box-technno">
-                <div class="container">
-                    <div class="col-7 left">
-                        <p>Công nghệ cắt tế bào một nửa (Half-cut) là cắt tế bào thành hai phần riêng biệt bằng tia laser hồng ngoại có cường độ cao, do đó giảm một nửa dòng điện hoạt động. Tổn thất nhiệt trên ruy-băng sẽ giảm đáng kể và công suất của mô-đun tăng 2%. Độ tin cậy của mô-đun cũng được nâng cao.
-                        </p>
-                        <p>Sự kết hợp của công nghệ tế bào cắt một nửa (Half-cell) và mô-đun hai mặt (Bifacial) có thể khuếch đại độ lợi nhờ hiệu ứng giảm dòng điện.</p>
-                    </div>
-                    <div class="col-5 right">
-                        <img src="./images/tachnology10.png" alt="" class="img-techno">
-                    </div>
-                </div>
-            </div>
-
         </div>
-
         <!-- Map -->
         <div class="map-container center">
             <div class="block block2 row">
